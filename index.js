65524a68f89b90d8e25566ab8f03aee663408f71
carlosluis11 <crlosluis111@gmail.com>
2023-02-25 21:52:54 +0000

    Domain-Name-Generator

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 let pronoun = ['the', 'our', 'my', 'your'];
 let adj = ['great', 'big', 'first','fast'];
 let noun = ['jogger', 'racoon', 'domain','page'];
+let extesions = ['.com','.net','.es','.pt']
 
 for (let word of pronoun) {
 for (let word2 of adj) {
 for(let word3 of noun){
-    console.log(word + word2 + word3 + ".com")
-    console.log(word + word2 + word3 + ".net")
+for(let word4 of extesions){
+    console.log(word + word2 + word3 + word4)
+}
 }
 }
 }
